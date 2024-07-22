3a4e71e41cee2975c212c34a5053997ffee5ad1c
Yojhan Garcia <ygarcia-2022194@kinal.edu.gt>
2024-07-22 12:58:42 -0600

    Cambios

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -7,7 +7,7 @@ export const NewsCard = ({ news }) => {
       <img src={news.imageUrl} alt={news.title} className="news-image" />
       <div className="news-content">
         <h2 className="news-title">{news.title}</h2>
-        <p className="news-description">{news.description}</p>
+        <p className="news-description">{news.content}</p>
         <p className="news-author">By {news.author}</p>
         <p className="news-date">{new Date(news.publishedDate).toLocaleDateString()}</p>
       </div>
