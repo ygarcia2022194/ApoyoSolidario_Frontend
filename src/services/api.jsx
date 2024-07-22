19865fbc5d8834c4a55a7a2d65095a8809aa29f1
Yojhan Garcia <ygarcia-2022194@kinal.edu.gt>
2024-07-22 10:44:43 -0600

    Cambios para desplegar front

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:3000/apoyoSolidario/v1',
+    baseURL: 'https://apoyo-solidario-backend.vercel.app/apoyoSolidario/v1',
     timeout: 5000,
 })
 
